380e320e2f28ef7f9dcb320dbd3557b56520c1fa
Eduardo José <duduxss3@gmail.com>
2021-10-15 20:11:55 -0300

    master
    	:tada: Begin of the project

---
+++ src/test/java/com/edudev/mailms/MailMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.mailms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
